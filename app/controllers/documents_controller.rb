0bf2d8d3fd7f800a24c357fc864b19854b24f65d
Chris Roos <chris@seagul.co.uk>
2026-03-16 15:27:27 +0000

    Add Documents#show pages

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,9 @@
 class DocumentsController < ApplicationController
   def index
-    @documents = Document.select(:title)
+    @documents = Document.select(:id, :title)
+  end
+
+  def show
+    @document = Document.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "documents#index"
+
+  resources :documents, only: [:show]
 end
